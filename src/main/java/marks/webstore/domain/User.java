4561adb059498a074bc2707b4a9c12a9752aca3b
MarkSmirnov13 <marksmirnov13@gmail.com>
2018-11-18 22:29:14 +0300

    add email conformation AND validation AND some cool stuff with design and UX/UI!!!

--- src/main/java/marks/webstore/domain/User.java
+++ src/main/java/marks/webstore/domain/User.java
@@ -11,6 +11,11 @@ public class User {
     private Long id;
     private String username;
     private String password;
+    private String name;
+    private String surname;
+    private String city;
+    private String email;
+    private String activationCode;
     private boolean active;
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
@@ -60,5 +65,45 @@ public class User {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
     }
 }
--- src/main/resources/templates/userEdit.ftl
+++ src/main/resources/templates/userEdit.ftl
@@ -3,7 +3,7 @@
 <h2 class="mb-3">User editor</h2>
 
 <form action="/user" method="post">
-    <input type="text" name="username" style="width: 18rem;" class="form-control mt-3 mb-2" value="${user.username}">
+    <input type="text" name="username" style="width: 18rem;" class="form-control mt-3 mb-2" value="${user.username}" placeholder="Name">
     <#list roles as role>
         <div class="custom-control custom-checkbox">
             <input type="checkbox" name="${role}" ${user.roles?seq_contains(role)?string("checked", "")} class="custom-control-input" id="${role}">
@@ -14,4 +14,5 @@
     <input type="hidden" value="${_csrf.token}" name="_csrf">
     <button type="submit" class="btn btn-light mt-2" style="width: 120px">Save</button>
 </form>
+
 </@C.page>
