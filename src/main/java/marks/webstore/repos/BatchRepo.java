2f73c9c59d5f2b503234733accfe6e9e4cfd33b5
LukianchukMaxim <lukianchukma@gmail.com>
2018-12-07 02:21:34 +0300

    DELETE EVRNG

--- src/main/java/marks/webstore/repos/BatchRepo.java
+++
@@ -1,7 +0,0 @@
-package marks.webstore.repos;
-
-import marks.webstore.domain.Batch;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BatchRepo extends CrudRepository<Batch, Long> {
-}
