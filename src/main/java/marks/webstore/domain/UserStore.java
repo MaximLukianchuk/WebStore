2ee193ba7db91d9be64242048421bc68c7c59f3d
LukianchukMaxim <lukianchukma@gmail.com>
2018-12-09 18:10:11 +0300

    [Product editor] + [userStore table] + [fixed some bugs]

---
+++ src/main/java/marks/webstore/domain/UserStore.java
@@ -0,0 +1,46 @@
+package marks.webstore.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UserStore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    public UserStore() {
+    }
+
+    public UserStore(User user, Store store) {
+        this.user = user;
+        this.store = store;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+}
